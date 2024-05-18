8820b8f3709bd92928cec7f807d4f3425c81a367
nsanikitin <nsa010995@gmail.com>
2024-05-18 17:57:40 +0300

    Создан PersistentContainerCreator для работы с CoreData

---
+++ Tracker/CoreData/NSPersistentContainer.swift
@@ -0,0 +1,28 @@
+import CoreData
+import Foundation
+
+final class PersistentContainerCreator {
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TrackerModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    private init() {}
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                context.rollback()
+            }
+        }
+    }
+}

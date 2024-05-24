e09755fdd968b8e0afd955959ca2d0aba8aae6ba
nsanikitin <nsa010995@gmail.com>
2024-05-24 18:28:01 +0300

    Реализован экран онбординга при первом запуске приложения

---
+++ Tracker/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,134 @@
+import UIKit
+
+final class OnboardingViewController: UIPageViewController {
+    
+    // MARK: - Properties
+    
+    private lazy var exitButton = {
+        let button = UIButton()
+        button.backgroundColor = .ypBlack
+        button.setTitle("Вот это технологии!", for: .normal)
+        button.setTitleColor(.ypWhite, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: ViewConfigurationConstants.buttonFontSize)
+        
+        button.addTarget(self, action: #selector(exitButtonDidTap), for: .touchUpInside)
+        
+        return button
+    }()
+    private lazy var pages: [UIViewController] = {
+        let bluePage = OnboardingPageViewController(
+            screenImage: UIImage(named: "onboardingScreenPage1"),
+            labelText: "Отслеживайте только то, что хотите"
+        )
+        let redPage = OnboardingPageViewController(
+            screenImage: UIImage(named: "onboardingScreenPage2"),
+            labelText: "Даже если это не литры воды и йога"
+        )
+        
+        return [bluePage, redPage]
+    }()
+    private lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = pages.count
+        pageControl.currentPage = 0
+        
+        pageControl.currentPageIndicatorTintColor = .ypBlack
+        pageControl.pageIndicatorTintColor = .ypBlack.withAlphaComponent(0.3)
+        
+        return pageControl
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        delegate = self
+        setViewControllers([pages[0]], direction: .forward, animated: true)
+        
+        setupExitButton()
+        setupPageControl()
+    }
+    
+    // MARK: - Onboarding View Configuration
+    
+    private func setupExitButton() {
+        exitButton.layer.masksToBounds = true
+        exitButton.layer.cornerRadius = ViewConfigurationConstants.elementsCornerRadius
+        
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(exitButton)
+        
+        NSLayoutConstraint.activate([
+            exitButton.heightAnchor.constraint(equalToConstant: ViewConfigurationConstants.buttonHeight),
+            exitButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            exitButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    private func setupPageControl() {
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pageControl)
+        
+        NSLayoutConstraint.activate([
+            pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pageControl.bottomAnchor.constraint(equalTo: exitButton.topAnchor, constant: -24)
+        ])
+    }
+    
+    // MARK: - Actions
+    
+    @objc
+    private func exitButtonDidTap() {
+        UserDefaults.standard.set("onboardingIsCompleted", forKey: "onboarding")
+        let TabBarController = TabBarController()
+        TabBarController.modalPresentationStyle = .fullScreen
+        present(TabBarController, animated: false)
+    }
+
+}
+
+// MARK: - PageViewController Extensions
+
+extension OnboardingViewController: UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if let currentViewController = pageViewController.viewControllers?.first,
+           let currentIndex = pages.firstIndex(of: currentViewController) {
+            pageControl.currentPage = currentIndex
+        }
+    }
+}
+
+extension OnboardingViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+
+        guard previousIndex >= 0 else {
+            return pages.last
+        }
+
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+            return nil
+        }
+
+        let nextIndex = viewControllerIndex + 1
+
+        guard nextIndex < pages.count else {
+            return pages.first
+        }
+
+        return pages[nextIndex]
+    }
+}
--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -1,23 +1,29 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = TabBarController()
+        
+        if UserDefaults.standard.string(forKey: "onboarding") == "onboardingIsCompleted" {
+            window?.rootViewController = TabBarController()
+        } else {
+            window?.rootViewController = OnboardingViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        }
+        
         window?.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {}
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {}
-
+    
     func sceneWillResignActive(_ scene: UIScene) {}
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {}
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {}
 }
